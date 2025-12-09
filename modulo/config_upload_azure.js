891959cd66c3fa13cc99e48726624954b466bd93
KAUAN ANTUNES LIMA <25132733@CFP127ALUNO.LOCAL>
2025-12-09 16:28:00 -0300

    adicionando upload

---
+++ modulo/config_upload_azure.js
@@ -0,0 +1,14 @@
+/***************************************************************************************
+ * Objetivo: Arquivo de configuração do UPLOAD de arquivos na Azure
+ * Data: 09/12/2025
+ * Autor: Kauan
+ * Versão: 1.0
+ ***************************************************************************************/
+
+//const TOKEN     = 'sp=racwd&st=2025-10-28T11:26:48Z&se=2025-10-28T19:41:48Z&sv=2024-11-04&sr=c&sig=qt443p615EwW90OXSPD3KixCQZo8DKD0lxjY05LibY4%3D'
+const TOKEN     = 'sp=cw&st=2025-12-09T18:59:46Z&se=2025-12-10T03:14:46Z&spr=https&sv=2024-11-04&sr=c&sig=GbDmE3s9IeeQzwZ7B97uDpLAlrqbvUZxlyo1uHLPurI%3D'
+const ACCOUNT   = 'uploadfotoss'
+const CONTAINER = 'uploadfotos'
+
+
+module.exports = {TOKEN, ACCOUNT, CONTAINER}
