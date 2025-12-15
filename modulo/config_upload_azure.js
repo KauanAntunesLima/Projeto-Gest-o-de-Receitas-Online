15145c7d8fa8ecb96e71047d39f16604ac3102d8
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2025-12-15 14:17:49 -0300

    alterando token de upload da azure

--- modulo/config_upload_azure.js
+++ modulo/config_upload_azure.js
@@ -6,7 +6,7 @@
  ***************************************************************************************/
 
 //const TOKEN     = 'sp=racwd&st=2025-10-28T11:26:48Z&se=2025-10-28T19:41:48Z&sv=2024-11-04&sr=c&sig=qt443p615EwW90OXSPD3KixCQZo8DKD0lxjY05LibY4%3D'
-const TOKEN     = 'sp=rcw&st=2025-12-11T13:52:18Z&se=2025-12-16T22:07:18Z&spr=https&sv=2024-11-04&sr=c&sig=wb7Ut6YSC4MX8zGf91VetrazAxN5ZnObC9q53ElAL2Q%3D'
+const TOKEN     = 'sp=rw&st=2025-12-15T16:56:40Z&se=2025-12-19T01:11:40Z&spr=https&sv=2024-11-04&sr=c&sig=0Waa46TdQ6D895YCq9ZG6o98PIiPJ88YDtJrztSmfoA%3D'
 const ACCOUNT   = 'toquesenaistorage'
 const CONTAINER = 'tgsenai'
 
